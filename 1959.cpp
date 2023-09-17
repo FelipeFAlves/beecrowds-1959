42986dfff936630ddff83c968df777fb029e39d4
FelipeFAlves <felipeferreira@asimovjr.com.br>
2023-09-17 11:18:09 -0300

    beecrowds-1959
    beecrowds-1959

---
+++ 1959.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Felipe Ferreira Alves - 2021020653
+int main(){
+    unsigned int n,l;
+    scanf("%d %d",&n,&l);
+    unsigned int p = n*l;
+    printf("%u\n",p);
+
+    return 0;
+}
